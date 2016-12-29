9423b0b31d7cc031a34da7e059c29854aceaa228
Ben Ooms <benoomsdh@gmail.com>
2016-12-29 20:20:42 +0100

    Added test samples and Java 8 compiler settings

--- src/main/java/nl/benooms/spockarticle/CalculatorServiceImpl.java
+++ src/main/java/nl/benooms/spockarticle/CalculatorServiceImpl.java
@@ -1,11 +1,24 @@
 package nl.benooms.spockarticle;
 
+import java.util.Objects;
+
 /**
  * Created by booms on 29-12-16.
  */
 public class CalculatorServiceImpl  implements CalculatorService{
 
+    public static final String NO_OPERANDS_ERROR_MESSAGE = "at least one operand should be supplied";
+
     public int add(int... operands) {
-        return 0;
+        Objects.requireNonNull(operands, NO_OPERANDS_ERROR_MESSAGE);
+        int result = 0;
+        if (operands.length > 0){
+            for (int i : operands){
+                result += i;
+            }
+        } else {
+            throw new NullPointerException(NO_OPERANDS_ERROR_MESSAGE);
+        }
+        return result;
     }
 }
--- src/test/groovy/nl/benooms/spockarticle/CalculatorServiceSpec.groovy
+++ src/test/groovy/nl/benooms/spockarticle/CalculatorServiceSpec.groovy
@@ -1,6 +1,7 @@
 package nl.benooms.spockarticle
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by booms on 29-12-16.
@@ -9,8 +10,32 @@ class CalculatorServiceSpec extends Specification {
 
     def calculatorService = new CalculatorServiceImpl()
 
-    def "Add"() {
-        expect:
-            assert calculatorService.add() == 0
+    def "Add should throw message when operands is null" () {
+        when:
+            calculatorService.add(null)
+        then:
+            def error = thrown(NullPointerException.class)
+            assert error.message == "at least one operand should be supplied"
     }
+
+    def "Add should throw message when no operands are provided" () {
+        when:
+        calculatorService.add(new int[0])
+        then:
+        def error = thrown(NullPointerException.class)
+        assert error.message == "at least one operand should be supplied"
+    }
+
+    @Unroll
+    "When #operands are provided #result is expected" () {
+        expect:
+            assert calculatorService.add(operands) == result
+        where:
+        operands           || result
+        [1,2] as int[]     || 3
+        [3]  as int[]      || 3
+        [10,12,2] as int[] || 24
+    }
+
+
 }
